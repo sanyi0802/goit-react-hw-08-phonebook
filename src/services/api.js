099db5a12418dbd603c0c7c6905f58f061a0fa4b
Sandra <42647088+sanyi0802@users.noreply.github.com>
2024-06-12 20:04:04 -0500

    Revert "Update api.js"
    This reverts commit 8b6cdc9161774f6dce5ae631fa994bd98621bf9c.

--- src/services/api.js
+++ src/services/api.js
@@ -13,6 +13,7 @@ export const token = {
   },
 };
 
+// Auth endpoints
 export const register = async (userData) => {
   const response = await axios.post('/users/signup', userData);
   return response.data;
@@ -33,7 +34,7 @@ export const fetchCurrentUser = async () => {
   return response.data;
 };
 
-
+// Contact endpoints
 export const fetchContacts = async () => {
   const response = await axios.get('/contacts');
   return response.data;
